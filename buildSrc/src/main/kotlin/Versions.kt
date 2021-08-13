c82e77215296ab0255210fed752cb83e4e18b8be
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-08-13 19:46:26 -0300

    Bump version and Kotlin version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    const val PUDDING = "1.0.0-SNAPSHOT"
-    const val KOTLIN = "1.4.32"
-    const val KOTLIN_SERIALIZATION = "1.1.0"
+    const val PUDDING = "1.1.0-SNAPSHOT"
+    const val KOTLIN = "1.5.21"
+    const val KOTLIN_SERIALIZATION = "1.2.1"
     const val KTOR = "1.5.3"
     const val EXPOSED = "0.31.1"
 }
