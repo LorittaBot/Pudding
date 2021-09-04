5ff43b94f634485663d4d55c94683f1e55723ff6
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-09-03 21:15:00 -0300

    Add request logging and log invalid authentication requests

--- server/src/main/kotlin/net/perfectdreams/loritta/pudding/server/Pudding.kt
+++ server/src/main/kotlin/net/perfectdreams/loritta/pudding/server/Pudding.kt
@@ -1,16 +1,24 @@
 package net.perfectdreams.loritta.pudding.server
 
 import io.ktor.application.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.util.*
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import net.perfectdreams.loritta.pudding.server.database.Database
 import net.perfectdreams.loritta.pudding.server.routes.api.v1.PostRPCRoute
 import net.perfectdreams.loritta.pudding.server.utils.config.PuddingConfig
 
 class Pudding(val config: PuddingConfig) {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+        private val TimeToProcess = AttributeKey<Long>("TimeToProcess")
+    }
+
     private val routes = listOf(
         PostRPCRoute(this)
     )
@@ -31,6 +39,26 @@ class Pudding(val config: PuddingConfig) {
                 get("/") {
                     call.respondText("Pudding!")
                 }
+            }
+
+            this.environment.monitor.subscribe(Routing.RoutingCallStarted) { call: RoutingApplicationCall ->
+                call.attributes.put(TimeToProcess, System.currentTimeMillis())
+                val userAgent = call.request.userAgent()
+                val ip = call.request.local.remoteHost
+                val queryString = "?" + call.request.queryString()
+                val httpMethod = call.request.httpMethod.value
+
+                logger.info { "$ip (${userAgent}): $httpMethod ${call.request.path()}${queryString}" }
+            }
+
+            this.environment.monitor.subscribe(Routing.RoutingCallFinished) { call: RoutingApplicationCall ->
+                val originalStartTime = call.attributes[TimeToProcess]
+                val userAgent = call.request.userAgent()
+                val ip = call.request.local.remoteHost
+                val queryString = "?" + call.request.queryString()
+                val httpMethod = call.request.httpMethod.value
+
+                logger.info { "$ip (${userAgent}): $httpMethod ${call.request.path()}${queryString} - OK! ${System.currentTimeMillis() - originalStartTime}ms" }
             }
         }
 
--- server/src/main/kotlin/net/perfectdreams/loritta/pudding/server/routes/api/v1/RequiresAuthenticationAPIv1Route.kt
+++ server/src/main/kotlin/net/perfectdreams/loritta/pudding/server/routes/api/v1/RequiresAuthenticationAPIv1Route.kt
@@ -4,6 +4,7 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
+import mu.KotlinLogging
 import net.perfectdreams.loritta.pudding.common.endpoints.Endpoint
 import net.perfectdreams.loritta.pudding.common.endpoints.EndpointMethod
 import net.perfectdreams.loritta.pudding.common.endpoints.Endpoints
@@ -12,12 +13,19 @@ import net.perfectdreams.loritta.pudding.server.Pudding
 import net.perfectdreams.loritta.pudding.server.routes.api.VersionedAPIRoute
 
 abstract class RequiresAuthenticationAPIv1Route(endpoint: Endpoint, val m: Pudding) : VersionedAPIRoute(EndpointsV1, endpoint.path) {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
+
+
     override suspend fun onRequest(call: ApplicationCall) {
         val authorization = call.request.authorization()
         if (authorization in m.config.keys)
             onAuthenticatedRequest(call)
-        else
+        else {
+            logger.warn { "Rejected request due to unknown key! Key: \"$authorization\"" }
             call.respondText("", ContentType.Text.Html, HttpStatusCode.Unauthorized)
+        }
     }
 
     abstract suspend fun onAuthenticatedRequest(call: ApplicationCall)
